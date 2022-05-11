151a15911a821009aeda3c7d37d5920e9260b3d8
nethramj <100124802+nethramj@users.noreply.github.com>
2022-05-11 14:59:38 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.cg.memberapplication;
+
+	import java.text.ParseException;
+
+	public class Main {
+		public static void main(String[] args)throws ParseException {
+			
+			Library lib = new Library();
+			lib.menu();
+		}
+}
---
+++ Member.java
@@ -0,0 +1,5 @@
+package com.cg.memberapplication;
+
+public class Member {
+
+}
